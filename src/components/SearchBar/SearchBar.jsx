e30f0e1126a2ccc77883db466257075037c17032
Siyu Liu <siyu.java.web@gmail.com>
2023-09-20 15:30:45 +1000

    adds functionalities for search bar and button to collect search terms

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SearchBar.module.scss'
 
-const SearchBar = () => {
+const SearchBar = ({ handleSubmit }) => {
+  const [input, setInput] = useState('');
+  const [selected, setSelected] = useState('title');
+  const onSubmit = (e) => { 
+    e.preventDefault();
+    handleSubmit({ input, selected });
+    console.log(input);
+    console.log(selected);
+    setInput('');
+  }
   return (
     <>
-      <form action="" className={ styles.form}>
+      <form className={ styles.form} onSubmit={onSubmit}>
         <div>
-          <input type="text" className={styles.input} />
-          <select name="" id="" className={styles.select}>
+          <input type="text" className={styles.input} value={input} onChange={e=>setInput(e.target.value)}/>
+          <select name="" id="" className={styles.select} value={selected} onChange={e=>setSelected(e.target.value)}>
             <option value="title">By Title</option>
             <option value="author">By Author</option>
             <option value="publisher">By Publisher</option>
