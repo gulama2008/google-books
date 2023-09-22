92cea6be29914d4a93c7be76cc6a4703d85a5372
Siyu Liu <siyu.java.web@gmail.com>
2023-09-22 23:13:21 +1000

    creates book details modal and adds onclick funtions to show and hide it

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -4,16 +4,23 @@ import styles from "./BookCard.module.scss";
 const BookCard = ({ book }) => {
   const bookInfo = book.volumeInfo;
   console.log(bookInfo);
+
   const [isModalDisplay, setIsModalDisplay] = useState(false);
-
-  const openModal = () => { 
+  console.log(isModalDisplay);
+  const openModal = () => {
+    console.log(isModalDisplay);
     setIsModalDisplay(true);
-  }
+  };
 
-  const closeModal = () => { 
+  const closeModal = (e) => {
+    console.log("close modal");
+    console.log(isModalDisplay);
     setIsModalDisplay(false);
-  }
-
+    e.stopPropagation();
+    console.log('testtest');
+    console.log(isModalDisplay);
+  };
+  console.log(isModalDisplay,'test');
 
   return (
     <div className={styles.card} onClick={openModal}>
@@ -27,15 +34,15 @@ const BookCard = ({ book }) => {
         className={styles.img}
       />
       <div className={styles.info}>
-        <p className={styles.title}>
-          {bookInfo.title}
-        </p>
-        <p className={styles.author}>
-          By {bookInfo.authors}
-        </p>
+        <p className={styles.title}>{bookInfo.title}</p>
+        <p className={styles.author}>By {bookInfo.authors}</p>
       </div>
 
-      {isModalDisplay&&<div className={styles.modal}>
+      <div
+        className={[styles.modal, isModalDisplay && styles.show_modal].join(
+          " "
+        )}
+      >
         <div onClick={closeModal}>X</div>
         <img
           src={
@@ -60,19 +67,19 @@ const BookCard = ({ book }) => {
             {bookInfo.description}
           </p>
           <p>
-            <span>Category: </span> {bookInfo.categories}
-          </p>
-          <p>
             <span>Language: </span> {bookInfo.language}
           </p>
           <p>
             <span>PublishedDate: </span> {bookInfo.publishedDate}
           </p>
           <p>
+            <span>Preview Link: </span> <a href={bookInfo.previewLink}>Preview</a>
+          </p>
+          <p>
             <span>Publisher: </span> {bookInfo.publisher}
           </p>
         </div>
-      </div>}
+      </div>
     </div>
   );
 };
